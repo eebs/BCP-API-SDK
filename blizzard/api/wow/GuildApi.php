0f16a0313c6ed2e10518726a796f02dd398e2408
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-08-22 16:36:11 +0000

    Added GuildApi.

--- blizzard/api/wow/GuildApi.php
+++ blizzard/api/wow/GuildApi.php
@@ -1,1 +1,65 @@
 <?php
+
+namespace blizzard\api\wow;
+
+use \blizzard\api\wow\WowApiAbstract;
+use \blizzard\api\wow\WowApiException;
+
+include_once 'WowApiAbstract.php';
+include_once 'WowApiException.php';
+
+/**
+ * @todo
+ *
+ * @author		Ebrahim Kobeissi
+ * @copyright	Ebrahim Kobeissi
+ * @version		0.1.0
+ * @package		blizzard.api.wow
+ */
+class GuildApi extends WowApiAbstract {
+
+	/**
+	 * Append guild and realm to API URL.
+	 *
+	 * @access public
+	 * @param array $config
+	 * @return void
+	 * @constructor
+	 */
+	public function __construct(array $config = array()) {
+		if (empty($config['guild']) || empty($config['realm'])) {
+			throw new WowApiException('Please provide a guild name and realm.');
+		}
+
+		$config['guild'] = utf8_encode(str_replace(' ', '%20', $config['guild']));
+
+		parent::__construct($config);
+
+		$this->setApiUrl($this->getApiUrl() . sprintf('guild/%s/%s/', $this->_config['realm'], $this->_config['guild']));
+	}
+
+	/**
+	 * Store a cache of the base results. We will use this cache to filter upon
+	 * instead of doing subsequent HTTP requests.
+	 *
+	 * @access public
+	 * @return boolean
+	 * @final
+	 */
+	final public function cache() {
+		if ($this->getCacheEngine()->has(self::CACHE_KEY)) {
+			return true;
+		}
+
+		$request = $this->request();
+		$results = $request->response();
+
+		if (!empty($results)) {
+			$this->getCacheEngine()->set(self::CACHE_KEY, $results);
+			return true;
+		}
+
+		return false;
+	}
+
+}
