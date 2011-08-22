4c61b6f2a68da7146532a6a388e77384d87ec2f8
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-08-22 16:35:43 +0000

    Added CharacterApi.

--- blizzard/api/wow/CharacterApi.php
+++ blizzard/api/wow/CharacterApi.php
@@ -2,7 +2,7 @@
 /**
  * Copyright (c) 2010-2011 Blizzard Entertainment
  *
- * Permission is herefilterBy granted, free of charge, to any person obtaining a copy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -48,13 +48,38 @@ class CharacterApi extends WowApiAbstract {
 	 * @constructor
 	 */
 	public function __construct(array $config = array()) {
-		/*if (empty($config['character']) || empty($config['realm'])) {
+		if (empty($config['character']) || empty($config['realm'])) {
 			throw new WowApiException('Please provide a character name and realm.');
 		}
 
+		$config['character'] = utf8_encode($config['character']);
+
 		parent::__construct($config);
 
-		$this->setApiUrl($this->getApiUrl() . sprintf('character/%s/%s/', $config['realm'], $config['character']));*/
+		$this->setApiUrl($this->getApiUrl() . sprintf('character/%s/%s/', $this->_config['realm'], $this->_config['character']));
 	}
 
+	/**
+	 * Store a cache of the base results. We will use this cache to filter upon
+	 * instead of doing subsequent HTTP requests.
+	 *
+	 * @access public
+	 * @return boolean
+	 * @final
+	 */
+	final public function cache() {
+		if ($this->getCacheEngine()->has(self::CACHE_KEY)) {
+			return true;
+		}
+
+		$request = $this->request();
+		$results = $request->response();
+
+		if (!empty($results)) {
+			$this->getCacheEngine()->set(self::CACHE_KEY, $results);
+			return true;
+		}
+
+		return false;
+	}
 }
